4cd24eb16cb920b0cb3cb853472d5f7ffe1cba32
Luca <lucacrisafi@gmail.com>
2025-08-10 20:34:22 +0100

    Update Zsh config

--- Modules/Zsh.nix
+++ Modules/Zsh.nix
@@ -3,5 +3,26 @@
 {
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
-  programs.zsh.enable = true;
+  programs = {
+    zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      enableLsColors = true;
+      shellInit = "fastfetch";
+      histSize = 1000;
+      shellAliases = {
+        update = "sudo nixos-rebuild switch -L --refresh --flake github:Boxingflame/nixos";
+        updateb = "sudo nixos-rebuild boot -L --refresh --flake github:Boxingflame/nixos";
+      };
+      ohMyZsh = {
+        enable = true;
+        theme = "gallois";
+        plugins = [
+          "git"
+          "history"
+        ];
+      };
+    };
+  };
 }
