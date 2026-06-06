61f4e6159e499a59ebe63f4139bb4289db978f8f
Luca <trifle-slinky-ouch@duck.com>
2026-06-06 01:29:52 +0100

    kill ollama

--- Modules/Services.nix
+++ Modules/Services.nix
@@ -1,22 +1,22 @@
 { config, pkgs-unstable, ... }:
 
 {
-  services = {
-     ollama = {
-      enable = false;
-      rocmOverrideGfx = "10.3.0";
-      # environmentVariables = {
-      #   OLLAMA_DEBUG = "1";
-      # };
-      loadModels = [ 
-        "llama3.1:8b"
-        "llama3.2:3b"
-        "deepseek-r1:8b"
-        "gpt-oss:20b"
-      ];
-      package = pkgs-unstable.ollama-rocm;
-    };
+  # services = {
+  #    ollama = {
+  #     enable = false;
+  #     rocmOverrideGfx = "10.3.0";
+  #     # environmentVariables = {
+  #     #   OLLAMA_DEBUG = "1";
+  #     # };
+  #     loadModels = [ 
+  #       "llama3.1:8b"
+  #       "llama3.2:3b"
+  #       "deepseek-r1:8b"
+  #       "gpt-oss:20b"
+  #     ];
+  #     package = pkgs-unstable.ollama-rocm;
+  #   };
     
-    open-webui.enable = true;
-  };
+  #   open-webui.enable = true;
+  # };
 }
