dbdbdbe9398b58c366e3e45064c3991dfd525279
Luca <trifle-slinky-ouch@duck.com>
2026-07-07 23:48:32 +0100

    fuck rockstar

--- Modules/Networking.nix
+++ Modules/Networking.nix
@@ -15,5 +15,10 @@
       ];
       allowedUDPPorts = [25565];
     };
+    extraHosts = '' 
+      127.0.0.1 paradise-s1.battleye.cnom
+      127.0.0.1 test-s1.battleye.com
+      127.0.0.1 paradiseenhanced-s1.battleye.com
+    '';
   };
 }
